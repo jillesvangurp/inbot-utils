0549e2751169c25705484c257253573d1f62bece
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2018-03-26 11:23:56 +0200

    clarify javadoc

--- src/test/java/io/inbot/utils/PatternEvaluatorTest.java
+++ src/test/java/io/inbot/utils/PatternEvaluatorTest.java
@@ -1,18 +1,20 @@
 package io.inbot.utils;
 
-import static org.assertj.core.api.StrictAssertions.assertThat;
-
 import org.testng.annotations.Test;
+
+import static io.inbot.utils.PatternEvaluator.evaluator;
+import static io.inbot.utils.PatternEvaluator.matches;
+import static org.assertj.core.api.StrictAssertions.assertThat;
 
 @Test
 public class PatternEvaluatorTest {
 
     public void shouldEvalPatterns() {
-        PatternEvaluator<Integer, String> evaluator = PatternEvaluator.evaluator(
+        PatternEvaluator<Integer, String> evaluator = evaluator(
             PatternEvaluator.equals(1,input->"one"),
             PatternEvaluator.equals(2,input->"two"),
             PatternEvaluator.equals(3,input->"three"),
-            PatternEvaluator.matches(input -> input>=3,input->"big")
+            matches(input -> input>=3,input->"big")
         );
         assertThat(evaluator.evaluate(2).get()).isEqualTo("two");
         assertThat(evaluator.evaluate(20).get()).isEqualTo("big");
